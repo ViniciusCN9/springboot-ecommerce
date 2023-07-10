e7115204beccb53dcfa0d69973bd65985269df96
ViniciusCN9 <vinicius.engenhariacn@gmail.com>
2023-07-10 00:03:53 -0300

    Remove ordenação de teste

--- src/test/java/com/dev/ecommerce/repositories/ProductRepositoryTest.java
+++ src/test/java/com/dev/ecommerce/repositories/ProductRepositoryTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
